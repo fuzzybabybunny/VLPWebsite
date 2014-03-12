4e6102d79f658eaed3a6a09928387907af3c2869
Victor Lin <victor@victorlinphoto.com>
2014-03-12 21:34:29 +0800

    Deleted a ton of stuff

--- sandbox/wordpress/wp-content/themes/MedicalDoctor/searchform.php
+++
@@ -1,3 +0,0 @@
-<form method="get" id="searchform" action="<?php echo home_url(); ?>/">
-	<input type="text" value="To search type and hit enter" onfocus="if(this.value=='To search type and hit enter')this.value='';" onblur="if(this.value=='')this.value='To search type and hit enter';" name="s" id="s3" class="search-input" />
-</form>
